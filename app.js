ef941aee44d7342c60cf0d7daa67b3eabc8c4eaf
iamsoomini <gsssm94@gmail.com>
2024-03-06 21:21:18 +0900

    username 받기

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 const loginForm = document.getElementById("#login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting"); 
+
+const HIDDEN_CLASSNAME = "hidden";
+
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add("hidden");
+    const userName = loginInput.value;
+    console.log(userName);
+
+    greeting.innerText = "Hello " + userName;
+    greeting.innerText = `Hello ${username}`;
+    
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
+    // if(userName === ""){
+    //     alert("please write your name.");
+    // }else if(userName.length > 15){
+    //     alert("your name is too long.")
+    // }
+    
+}
+
+// function handleLinkClick(event){
+//     //이벤트가 작동하지 않도록 막음.
+//     event.preventDefault();
+//     console.log(event);
+//     alert("clicked!");
+
+// }
+
+loginForm.addEventListener("submit", onLoginSubmit);
